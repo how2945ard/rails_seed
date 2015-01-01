8bc9e2eabcbaaede4fd014ac154abbb3fe2f3d1d
Howard <how2945ard@gmail.com>
2015-01-02 02:05:12 +0800

    oauth

---
+++ app/controllers/callbacks_controller.rb
@@ -0,0 +1,26 @@
+class CallbacksController < Devise::OmniauthCallbacksController
+  def facebook
+     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
+     if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      puts '!!!!!!!!!!!!!!'
+    else
+      puts "??????????????"
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      sign_in_and_redirect @user
+    end
+  end
+  def google_oauth2
+     @user = User.find_for_google_oauth2_oauth(request.env["omniauth.auth"], current_user)
+     if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
+      puts '!!!!!!!!!!!!!!'
+    else
+      puts "??????????????"
+      session["devise.google_data"] = request.env["omniauth.auth"]
+      sign_in_and_redirect @user
+    end
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,50 @@
+class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable,
+    :omniauthable, :omniauth_providers => [:facebook,:google_oauth2]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    if user
+      return user
+    else
+      registered_user = User.where(:email => auth.info.email).first
+      if registered_user
+        return registered_user
+      else
+        user = User.create(name:auth.extra.raw_info.name,
+                           provider:auth.provider,
+                           uid:auth.uid,
+                           email:auth.info.email,
+                           password:Devise.friendly_token[0,20],
+                           )
+      end
+    end
+  end
+  def self.find_for_google_oauth2_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    if user
+      return user
+    else
+      registered_user = User.where(:email => auth.info.email).first
+      if registered_user
+        return registered_user
+      else
+        user = User.create(name:auth.extra.raw_info.name,
+                           provider:auth.provider,
+                           uid:auth.uid,
+                           email:auth.info.email,
+                           password:Devise.friendly_token[0,20],
+                           )
+      end
+    end
+  end
+end
