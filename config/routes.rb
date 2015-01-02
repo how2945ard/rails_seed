720d06b382fd4137463f263933aeaae8fd88833a
Howard <how2945ard@gmail.com>
2015-01-02 19:57:45 +0800

    Revert "comfirm"
    This reverts commit 4fef87abfb2edcefb06532ac3764b00e02cb4933.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get "/logout" => "devise/sessions#destroy"
+  end
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   root 'index#index'
+  get '/apple' => 'index#apple'
 end
